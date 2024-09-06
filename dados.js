1cefd79a57d29ccf1fd02e097aef9eb270ad0957
DanielPasqualino <danipasqualino@gmail.com>
2024-09-06 17:08:36 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+   {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a medalha de bronze nos Jogos Olímpicos de Tóquio 2020.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk"
+    } ,
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas competições nacionais e internacionais. Conquistou diversas medalhas em campeonatos importantes e é considerada uma das maiores promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", // Substitua por um link válido para a Beatriz Souza
+        tags: "jodo judô judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Nos Jogos Olímpicos de Tóquio 2020, conquistou a medalha de prata no individual geral, sendo a primeira brasileira a conquistar uma medalha nessa modalidade.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade", // Substitua por um link válido para a Rebeca Andrade
+        tags: "ginástica ouro"
+    }
+]
